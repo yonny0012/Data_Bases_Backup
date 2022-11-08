1fe5d7055326a02a703e7ae88e8315bb3bfd6de2
Yonny Dominguez <yonnydominguez22@gmail.com>
2022-11-08 13:38:55 -0500

    Modificacion con ayida de Yan Carlos

--- Lab_ 5-Yonny-304 (estudiantes db).sql
+++ Lab_ 5-Yonny-304 (estudiantes db).sql
@@ -8,21 +8,64 @@
     la posición de la 5 a la 10, la nueva categoría será: Auxiliar. Muestre los 
     datos de todos los profesores, incluyendo las modificaciones.
 */
-CREATE OR REPLACE FUNCTION mostrarProfesores() returns void as
+
+--EL "SETOF" es para cuando voy a retornar varias filas
+CREATE OR REPLACE FUNCTION mostrarProfesores() returns SETOF profesor as
 $$
 DECLARE
-    variable record;
-    curs1 CURSOR FOR 
-        SELECT nombprof as nombre from profesor
-        WHERE idprof BETWEEN 5 AND 10
-        ORDER BY idprof;
+    var profesor%ROWTYPE;
+    curs1 CURSOR FOR
+        (SELECT nombprof as nombre from profesor
+        WHERE idprof
+        ORDER BY idprof);
+
+    cont INTEGER = 5;
+        
 BEGIN
     OPEN curs1;
-    FETCH cursr1 into variable;
+    MOVE ABSOLUTE 5 FROM curs1;
+    LOOP
+        IF (cont<=10) THEN
+            UPDATE profesor SET categoria = "Profesor Auxiliar" WHERE CURRENT OF curs1;
+            MOVE RELATIVE 1 FROM curs1;
+        ELSE
+            EXIT;
+        END IF;
+        cont = cont + 1;
+    END LOOP
+    CLOSE curs1;
+    OPEN curs1;
+
+    LOOP
+        FETCH curs1 INTO var;
+        RETURN NEXT var;
+        IF NOT FOUND THEN 
+            EXIT;
+        END IF;
+    END LOOP;
     CLOSE curs1;
 END
 $$
-LANGUAGE plpgsql;
-SELECT nombprof as nombre from profesor
+LANGUAGE 'plpgsql';
+SELECT * from profesor
     WHERE idprof BETWEEN 5 AND 10
     ORDER BY idprof;
+
+
+
+-- CASO1 : Uso simple de cursores EXPLÍCITOS 
+CREATE OR REPLACE FUNCTION expl_cursor1() RETURNS SETOF profesor AS
+$BODY$
+DECLARE
+    -- Declaración EXPLICITA del cursor
+    cur_clientes CURSOR FOR SELECT * FROM profesor; 
+    registro profesor%ROWTYPE;
+BEGIN
+   -- Procesa el cursor
+   FOR registro IN cur_clientes LOOP
+       RETURN NEXT registro;
+   END LOOP;
+   RETURN;
+END $BODY$ LANGUAGE 'plpgsql';
+
+SELECT expl_cursor1(); 
